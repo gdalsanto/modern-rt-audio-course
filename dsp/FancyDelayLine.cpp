8dfc70afcd7a175cf1edbe0b1d395e5996002498
Gloria Dal Santo <gloria.dalsanto@outlook.it>
2023-08-31 11:21:06 +0300

    Stereo delay line implementation

--- dsp/FancyDelayLine.cpp
+++ dsp/FancyDelayLine.cpp
@@ -29,8 +29,74 @@ void FancyDelayLine::prepare(unsigned int maxLengthSamples, unsigned int numChan
 }
 
 
-void FancyDelayLine::setDelaySamples(unsigned int newDelaySamples)
+void FancyDelayLine::setDelaySamples(unsigned int newDelaySamplesA, unsigned int newDelaySamplesB)
 {
-    delaySamples = std::max(std::min(newDelaySamples, static_cast<unsigned int>(delayBuffer[0].size() - 1u)), 1u);
+    delaySamplesA = std::max(std::min(newDelaySamplesA, static_cast<unsigned int>(delayBuffer[0].size() - 1u)), 1u);
+    delaySamplesB = std::max(std::min(newDelaySamplesB, static_cast<unsigned int>(delayBuffer[0].size() - 1u)), 1u);
+
 }
+
+void FancyDelayLine::process(float* const* audioOutput, const float* const* audioInput, const float* const* modInput, unsigned int numChannels, unsigned int numSamples)
+{
+    const unsigned int delayBufferSize{ static_cast<unsigned int>(delayBuffer[0].size()) };
+
+    numChannels = std::min(numChannels, static_cast<unsigned int>(delayBuffer.size()));
+    // for (unsigned int ch = 0; ch < numChannels; ++ch)
+    const int ch = 0;  // take first channel only, for now
+        // Calculate base indices based on fixed delay time
+    unsigned int workingWriteIndexA { writeIndexA };
+    unsigned int workingReadIndexA { (workingWriteIndexA + delayBufferSize - delaySamplesA) % delayBufferSize };
+    
+    unsigned int workingWriteIndexB { writeIndexB };
+    unsigned int workingReadIndexB { (workingWriteIndexB + delayBufferSize - delaySamplesB) % delayBufferSize };
+    
+    for (unsigned int n = 0; n < numSamples; ++n)
+    {
+        // Linear interpolation coefficients
+        const float m { std::fmax(modInput[ch][n], 0.f) };
+        const float mFloor { std::floor(m) };
+        const float mFrac0 { m - mFloor };
+        const float mFrac1 { 1.f - mFrac0 };
+
+        // Calculate read indices A
+        const unsigned int readIndexA0 { (workingReadIndexA + delayBufferSize - static_cast<unsigned int>(mFloor)) % delayBufferSize };
+        const unsigned int readIndexA1 { (readIndexA0 + delayBufferSize - 1u) % delayBufferSize };
+
+        // Read from delay line A
+        const float readA0 = delayBuffer[ch][readIndexA0];
+        const float readA1 = delayBuffer[ch][readIndexA1];
+        
+        // Calculate read indices B
+        const unsigned int readIndexB0 { (workingReadIndexB + delayBufferSize - static_cast<unsigned int>(mFloor)) % delayBufferSize };
+        const unsigned int readIndexB1 { (readIndexB0 + delayBufferSize - 1u) % delayBufferSize };
+        
+        // Read from delay line B
+        const float readB0 = delayBuffer[ch][readIndexB0];
+        const float readB1 = delayBuffer[ch][readIndexB1];
+
+        // Read audio input
+        const float x { audioInput[ch][n] };
+
+        // Interpolate stereo output
+        audioOutput[0][n] = readA0 * mFrac1 + readA1 * mFrac0;
+        audioOutput[1][n] = readB0 * mFrac1 + readB1 * mFrac0;
+        
+        // Write input
+        delayBuffer[ch][workingWriteIndexA] = x;
+
+        // Increament indices A
+        ++workingWriteIndexA; workingWriteIndexA %= delayBufferSize;
+        ++workingReadIndexA; workingReadIndexA %= delayBufferSize;
+
+        // Increament indices B
+        ++workingWriteIndexB; workingWriteIndexB %= delayBufferSize;
+        ++workingReadIndexB; workingReadIndexB %= delayBufferSize;
+    }
+
+    // Update persistent write index
+    writeIndexA += numSamples; writeIndexA %= delayBufferSize;
+    writeIndexB += numSamples; writeIndexB %= delayBufferSize;
 }
+
+}
+
